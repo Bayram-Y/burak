9b2f71d2638ae1c08fd52d04f504c4d3d8debfe8
Yaxiyaev Bayram <167451221+Bayram-Y@users.noreply.github.com>
2024-09-12 17:48:41 +0900

    Update train.ts

--- src/train.ts
+++ src/train.ts
@@ -1,37 +1,4 @@
 // TASK I:
-/*
-Shunday function tuzing, u parametrdagi array ichida eng ko'p
-takrorlangan raqamni topib qaytarsin.
-
-MASALAN: majorityElement([1, 2, 3, 4, 5, 4, 3, 4]); return 4
-
-Yuqoridag misolda argument sifatida kiritilayotgan array tarkibida 4 soni ko'p takrorlanganligi uchun 4'ni return qilmoqda.
-
-*/
-// function majorityElement(numberArr: number[]) {
-//   const sortedNum: number[] = numberArr.sort((a, b) => a - b);
-//   let mostFrequentNum: number = sortedNum[0];
-//   let maxCount: number = 1;
-//   let currentCount: number = 1;
-
-//   for (let i = 1; i < sortedNum.length; i++) {
-//     if (sortedNum[i] === sortedNum[i - 1]) {
-//       currentCount++;
-//     } else {
-//       currentCount = 1;
-//     }
-
-//     if (currentCount > maxCount) {
-//       maxCount = currentCount;
-//       mostFrequentNum = sortedNum[i];
-//     }
-//   }
-
-//   return mostFrequentNum;
-// }
-
-// const numbers: number[] = [1, 2, 3, 4, 5, 4, 3, 4];
-// console.log(majorityElement(numbers));
 
 function majorityElement(input: number[]) {
   let maxNum: number = 0;
