a9dbf9915add454ffe9776aebfd1ec254a50c750
Bayram-Y <bayramyaxiyaev7@gmail.com>
2024-09-14 18:00:38 +0900

    feat: completed MIT J TASK

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,31 @@
+// TASK J:
+/*
+Shunday function tuzing, u string qabul qilsin.
+Va string ichidagi eng uzun so'zni qaytarsin.
+MASALAN: findLongestWord("I came from Uzbekistan!"); return "Uzbekistan!"
+Yuqoridagi text tarkibida 'Uzbekistan'
+eng uzun so'z bo'lganligi uchun 'Uzbekistan'ni qaytarmoqda */
+
+function findLongestWord(word: string) {
+  const arr = word.split(" ");
+  return word.split(" ").reduce((longest, current) => {
+    return current.length > longest.length ? current : longest;
+  });
+}
+
+console.log(findLongestWord("I came from Uzbekistan!"));
+
+/*
+let uzunligi = 0;
+  let enUzun = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > uzunligi) {
+      let uzunligi = arr[i].length;
+      let uzun = arr[i];
+    }
+  }
+*/
 
 // TASK I:
 
