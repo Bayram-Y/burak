3523afd47f213e9e31c238184b2cf91b40089ba1
Bayram-Y <bayramyaxiyaev7@gmail.com>
2024-10-01 01:38:02 +0900

    fix: admin project refactoring

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -24,10 +24,8 @@ $(function () {
   });
 
   $(".new-product-status").on("change", async function (e) {
-    const id = e.target.id;
-    const productStatus = $(`#${id}.new-product-status`).val();
-    console.log("id:", id);
-    console.log("productStatus:", productStatus);
+    const id = e.target.id,
+      productStatus = $(`#${id}.new-product-status`).val();
 
     try {
       const response = await axios.post(`/admin/product/${id}`, {
