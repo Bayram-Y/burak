fc9a974ae5186cd69c0e03cd17a58507186e403a
Bayram-Y <bayramyaxiyaev7@gmail.com>
2024-10-20 16:13:56 +0900

    feat: develop getProducts rest Api

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -2,13 +2,38 @@ import { Request, Response } from "express";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";
-import { ProductInput } from "../libs/types/product";
+import { ProductInput, ProductInquiry } from "../libs/types/product";
 import { AdminRequest } from "../libs/types/member";
+import { ProductCollection } from "../libs/enums/product.enum";
 
 const productService = new ProductService();
 
 const productController: T = {};
 /**    SPA   **/
+productController.getProducts = async (req: Request, res: Response) => {
+  try {
+    console.log("getProducts");
+
+    const { page, limit, order, productCollection, search } = req.query;
+    const inquiry: ProductInquiry = {
+      order: String(order),
+      page: Number(page),
+      limit: Number(limit),
+    };
+    if (productCollection) {
+      inquiry.productCollection = productCollection as ProductCollection;
+    }
+    if (search) inquiry.search = String(search);
+
+    const result = await productService.getProducts(inquiry);
+
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("Error, getProducts:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standart.code).json(Errors.standart);
+  }
+};
 
 /**    SSR   **/
 
@@ -18,7 +43,7 @@ productController.getAllProducts = async (req: Request, res: Response) => {
     const data = await productService.getAllProducts();
     console.log("products:", data);
 
-    res.render("products", { products: data }); 
+    res.render("products", { products: data });
   } catch (err) {
     console.log("Error, signup:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
@@ -62,10 +87,10 @@ productController.updateChosenProduct = async (req: Request, res: Response) => {
     console.log("updateChosenProduct");
 
     const id = req.params.id;
-    const input = req.body
-    const result = await productService.updateChosenProduct(id, input); 
+    const input = req.body;
+    const result = await productService.updateChosenProduct(id, input);
 
-    res.status(HttpCode.OK).json({ data: result });  
+    res.status(HttpCode.OK).json({ data: result });
   } catch (err) {
     console.log("Error, updateChosenProduct:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
