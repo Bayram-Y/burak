0484be2ccf27d3eb5d68889c35c61ccbc9d4efd2
Bayram-Y <bayramyaxiyaev7@gmail.com>
2024-10-22 12:38:45 +0900

    feat: create view schema and service models

---
+++ src/models/View.service.ts
@@ -0,0 +1,10 @@
+import ViewModel from "../schema/View.model";
+
+class ViewService {
+  private readonly veiwModel;
+  constructor() {
+    this.veiwModel = ViewModel;
+  }
+}
+
+export default ViewService;
